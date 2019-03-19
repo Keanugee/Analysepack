4c024851b4942662fbb1b0b4a8f27dd47e4e9627
Keanugee <48674950+Keanugee@users.noreply.github.com>
2019-03-19 18:16:00 +0200

    Update sorting.py

--- Analysepack/sorting.py
+++ Analysepack/sorting.py
@@ -1,12 +1,12 @@
 #Bubble Sort
-def bubble_sort(items):
+def bubble_sort(items)
     for x in range(len(items)-1,0,-1):
         for i in range(x):
             if items[i] > items[i+1] :
                 temp = items[i]
-                items[i] = items[i+1]
+                items[i] = items[i+1] 
                 items[i+1] = temp
-    return items 
+    return items
 
 #Merge Sort
 def merge_sort(items):
